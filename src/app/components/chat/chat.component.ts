b29537cc1fde2c1e8676403da32a203cee6eca96
VZERT14 <vzert14@vzert-mac-2.local>
2019-01-22 12:22:35 -0600

    update for display extra-data
    declare new variables inside node_modules api-ai-javascript interfaces.ts

--- src/app/components/chat/chat.component.ts
+++ src/app/components/chat/chat.component.ts
@@ -10,23 +10,29 @@ import { log } from 'util';
   styleUrls: ['./chat.component.css']
 })
 export class ChatComponent {
-
+  
   conversation: IMessage[] = [];
 
   addMessageFromUser(message) {
     this.conversation.push({
       avatar: 'perm_identity',
       from: 'Me',
-      content: message.value
+      content: message.value,
+      imgRelevant:  null,
+      imageStatus: 'false'
     });
 
     client.textRequest(message.value).then((response) => {
 
-     
+     //console.log(response);
+      let status = 'false';
+      if(response.result.fulfillment.source == 'get-movie-details') status = 'true';
       this.conversation.push({
         avatar: 'android',
         from: 'Bot',
-        content: response.result.fulfillment['speech'] || 'I can\'t seem to figure that out!'
+        content: response.result.fulfillment['speech'] || 'I can\'t seem to figure that out!',
+        imgRelevant: response.result.fulfillment.messages[0].speech || null,  //change Interfaces.ts >interface IServerResponse, also declare any variable used in response
+        imageStatus: status
       });
       message.value = '';
     });
--- src/app/models/message.ts
+++ src/app/models/message.ts
@@ -1,5 +1,9 @@
+import { Type } from "@angular/compiler/src/output/output_ast";
+
 export interface IMessage {
     avatar: string;
     from: string;
     content: string;
+    imgRelevant : any;
+    imageStatus: string;
 }
